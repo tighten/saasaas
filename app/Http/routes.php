f7558050c52d5b5772931da048339e528feac338
Matt Stauffer <matt@tighten.co>
2015-06-24 16:36:49 -0400

    Add random word generator for later word

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,31 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    // $apiKey = getenv('WORDNIK_KEY');
+    $apiKey = 'a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5';
+
+    $url = sprintf(
+        "http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=false&includePartOfSpeech=noun-plural&minCorpusCount=1&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=%s",
+        $apiKey
+    );
+
+    $result = json_decode(file_get_contents($url));
+
+    $word = ucwords($result->word);
+
+    /* Later...
+
+    $url = sprintf(
+        "http://api.crunchbase.com/v/2/organizations?user_key=%s&order=updated_at%20desc",
+        $apiKey
+    );
+
+    $result = json_decode(file_get_contents($url));
+
+    dd($result);
+
+    $word = ucwords($result->word);
+    */
+
+    return "Uber for $word";
 });
