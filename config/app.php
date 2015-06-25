d69cf02a5caaad32d9fbbd4c96736711100c94fe
Matt Stauffer <matt@tighten.co>
2015-06-25 11:16:33 -0400

    Add image generation

--- config/app.php
+++ config/app.php
@@ -143,7 +143,7 @@ return [
         App\Providers\AppServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        Intervention\Image\ImageServiceProvider::class,
     ],
 
     /*
@@ -191,7 +191,7 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-
+        'Image'     => Intervention\Image\Facades\Image::class,
     ],
 
 ];
