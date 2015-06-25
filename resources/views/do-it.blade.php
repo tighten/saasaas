aa2229135d11a8845713e2f93492ef17d025d17c
Matt Stauffer <mattstauffer@users.noreply.github.com>
2015-06-25 09:15:35 -0400

    Google analytics you dolt

--- resources/views/do-it.blade.php
+++ resources/views/do-it.blade.php
@@ -81,5 +81,16 @@
                 <div class="actions"><a href="{{ url() }}">Pivot (try again)</a> | <a href="http://twitter.com/home?status={{ urlencode("We're pivoting! We're now going to be $startup for $word. - Generate your own pivot at http://saasaas.tighten.co/") }}">Tweet this</a></div>
             </div>
         </div>
+        
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-53203205-3', 'auto');
+  ga('send', 'pageview');
+
+</script>
     </body>
 </html>
