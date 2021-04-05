d8d792f729f1ed9ee7dc1e2d0354933b7caff270
Marcy Acevedo <marcy@tighten.co>
2021-04-05 18:25:17 -0500

    Update according to Shift instructions

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -16,9 +16,9 @@ class Kernel extends HttpKernel
     protected $middleware = [
         \App\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
-        \App\Http\Middleware\TrimStrings::class,
+        // \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\TrustProxies::class,
+        // \App\Http\Middleware\TrustProxies::class,
     ];
 
     /**
